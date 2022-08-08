c7893d267ca73bab601fb288186831055541bc44
Dimitry <lapynow.d@gmail.com>
2022-08-09 00:20:22 +0300

    dynamic import на примере RxJs

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import './res/css/index.styl';
 import './res/css/main.css';
 
 import $ from 'jquery';
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import {createRoot} from 'react-dom/client';
 
 
@@ -31,7 +31,28 @@ console.log(`–––> import XML`, xml);
 
 console.log(`–––> import CSV`, csv);
 
+let RxJs;
 const App = () => {
+
+    let [counter, setCounter] = useState(0);
+    let [isLoaded, setIsLoaded] = useState(false);
+
+    useLayoutEffect((...args) => {
+        console.log(`–––> useLayoutEffect`, args);
+
+        if (RxJs) {
+            RxJs.fromEvent(document.getElementById('incrementer'), 'click')
+                .subscribe(() => setCounter(n => n + 1));
+        }
+    }, [isLoaded])
+
+    function loadRxJs() {
+        import('rxjs').then((lib) => {
+            setIsLoaded(true);
+            RxJs = lib;
+        })
+    }
+
     return (
         <div>
             <h1>React test</h1>
@@ -46,6 +67,13 @@ const App = () => {
                     <li><label><input type="checkbox" defaultChecked={true}/> Подключение картинок</label></li>
                     <li><label><input type="checkbox" defaultChecked={true}/> Подключение бинарников (XML, CSV)</label></li>
                 </ol>
+            </div>
+
+            <div>
+                <button onClick={loadRxJs}>load RxJS</button>
+                <br/>
+                {isLoaded && <button id="incrementer">кликнуть</button>}
+                {isLoaded && counter}
             </div>
         </div>
     );
